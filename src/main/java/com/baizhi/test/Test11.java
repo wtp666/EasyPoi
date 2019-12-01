70aea06093bf809e606dfdd4d15f9860b7fddc95
wtp666 <2531249178@qq.com>
2019-12-01 11:47:34 +0800

    hahaha

--- src/main/java/com/baizhi/test/Test11.java
+++ src/main/java/com/baizhi/test/Test11.java
@@ -4,6 +4,7 @@ public class Test11 {
     public static void main(String[] args) {
         System.out.println("123456");
         System.out.println("666666");
+        System.out.println("hahah");
 
     }
 }
