f3dd7e9d64b3d07e901313a70426558ea4e34601
wtp666 <58296665+wtp666@users.noreply.github.com>
2019-12-01 11:16:25 +0800

    Update Test11.java

--- src/main/java/com/baizhi/test/Test11.java
+++ src/main/java/com/baizhi/test/Test11.java
@@ -3,5 +3,7 @@ package com.baizhi.test;
 public class Test11 {
     public static void main(String[] args) {
         System.out.println("123456");
+        System.out.println("666666");
+
     }
 }
